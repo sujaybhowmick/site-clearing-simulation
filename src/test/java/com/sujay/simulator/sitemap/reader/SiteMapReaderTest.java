929f550e2a9f421267ee58ce50e9a00c2124e35c
sbhowmick <sbhowmick@theorchard.com>
2021-08-05 09:31:20 +1000

    Unit test for SiteReader

--- src/test/java/com/sujay/simulator/sitemap/reader/SiteMapReaderTest.java
+++ src/test/java/com/sujay/simulator/sitemap/reader/SiteMapReaderTest.java
@@ -1,13 +1,14 @@
 package com.sujay.simulator.sitemap.reader;
 
 import com.sujay.simulator.sitemap.Cell;
+import com.sujay.simulator.sitemap.CellType;
 import com.sujay.simulator.sitemap.Coordinate;
 import com.sujay.simulator.sitemap.SiteMap;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import java.io.FileReader;
 import java.io.StringReader;
 
 class SiteMapReaderTest {
@@ -22,10 +23,37 @@ class SiteMapReaderTest {
     }
 
     @Test
-    void readMap() throws Exception {
+    void readMap() {
         final String input = "oooo\noror\ntoot";
         final MapReader<SiteMap> reader = builder.reader(new StringReader(input)).build();
         SiteMap siteMap = reader.readMap();
-        Cell cell = siteMap.getCellAt(new Coordinate(1, 2));
+        Coordinate coordinate11 = new Coordinate(1, 1);
+        Cell cell11 = siteMap.getCellAt(coordinate11);
+        assertThat(cell11).isNotNull();
+        assertThat(cell11.getCellType()).isEqualTo(CellType.ROCK);
+        assertThat(cell11.getCoordinate()).isEqualTo(coordinate11);
+
+        Coordinate coordinate23 = new Coordinate(2, 3);
+        Cell cell24 = siteMap.getCellAt(coordinate23);
+        assertThat(cell24).isNotNull();
+        assertThat(cell24.getCellType()).isEqualTo(CellType.TREE);
+        assertThat(cell24.getCoordinate()).isEqualTo(coordinate23);
+    }
+
+    @Test
+    void readMapOutofBounds() {
+        final String input = "oooo\noror\ntoot";
+        final MapReader<SiteMap> reader = builder.reader(new StringReader(input)).build();
+        SiteMap siteMap = reader.readMap();
+        Coordinate coordinate14 = new Coordinate(1, 4);
+        try {
+            Cell cell = siteMap.getCellAt(coordinate14);
+            assertThat(cell).isNotNull();
+            fail(String.format("Test failed as the bounds provided a cell value which is within bounds %s",
+                    coordinate14));
+        }catch (Exception iae) {
+            assertThat(true).isTrue();
+        }
+
     }
 }
