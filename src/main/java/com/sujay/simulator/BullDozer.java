42c9cdd21b883076c14e58fb03c09232ff007768
sbhowmick <sbhowmick@theorchard.com>
2021-08-06 21:23:41 +1000

    Added BullDozer class

--- src/main/java/com/sujay/simulator/BullDozer.java
+++ src/main/java/com/sujay/simulator/BullDozer.java
@@ -1,48 +1,75 @@
 package com.sujay.simulator;
 
 import com.sujay.simulator.command.Command;
-import com.sujay.simulator.sitemap.Coordinate;
-import com.sujay.simulator.sitemap.Orientation;
-import com.sujay.simulator.sitemap.SiteMap;
+import com.sujay.simulator.event.SimulationEvent;
+import com.sujay.simulator.sitemap.*;
 
-public class BullDozer {
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+
+public class BullDozer implements Runnable {
     private final SiteMap siteMap;
-    private Coordinate currentPosition = new Coordinate(0, 0);
+    private final BlockingQueue<SimulationEvent> eventQueue;
+    private final List<Cell> visitedCells = new ArrayList<>();
+    private Queue<Command> commandQueue;
+    private Cell currentCell = new Cell(CellType.START, new Coordinate(0, -1));
     private Orientation orientation = Orientation.EAST;
 
-    public BullDozer(SiteMap siteMap) {
+    public BullDozer(SiteMap siteMap, BlockingQueue<SimulationEvent> eventQueue) {
         this.siteMap = siteMap;
+        this.eventQueue = eventQueue;
     }
 
-    public void advance(Command command) {
+    public void setCommandQueue(Queue<Command> commandQueue) {
+        this.commandQueue = commandQueue;
+    }
+
+    private void execute(Command command) {
         command.execute();
     }
 
-    public void turnLeft(Command command){
-        command.execute();
-    }
-
-    public void turnRight(Command command) {
-        command.execute();
-    }
-
-    public Coordinate getCurrentPosition() {
-        return currentPosition;
+    public Cell getCurrentPosition() {
+        return currentCell;
     }
 
     public SiteMap getSiteMap() {
         return siteMap;
     }
 
+    public synchronized void updateCurrentPosition(Cell currentCell) {
+        this.currentCell = currentCell;
+    }
+
+    public synchronized void updateOrientation(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    public synchronized void addVisitedCell(Cell cell) {
+        this.visitedCells.add(cell);
+    }
+
+    public void addEvent(SimulationEvent event) {
+        try {
+            this.eventQueue.put(event);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public Orientation getOrientation() {
         return orientation;
     }
 
-    public void updateCurrentPosition(Coordinate currentPosition) {
-        this.currentPosition = currentPosition;
+    public List<Cell> getVisitedCells() {
+        return visitedCells;
     }
 
-    public void updateOrientation(Orientation orientation) {
-        this.orientation = orientation;
+    @Override
+    public void run() {
+        if(this.commandQueue != null) {
+            while(!this.commandQueue.isEmpty()) {
+                execute(this.commandQueue.poll());
+            }
+        }
     }
 }
