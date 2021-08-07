9da888c8808319eb0b02203333fc5330a71f3922
sujaybhowmick <sujaybhowmick@gmail.com>
2021-08-07 22:14:10 +1000

    added command methods

--- src/main/java/com/sujay/simulator/BullDozer.java
+++ src/main/java/com/sujay/simulator/BullDozer.java
@@ -1,19 +1,21 @@
 package com.sujay.simulator;
 
 import com.sujay.simulator.command.Command;
+import com.sujay.simulator.command.QuitCommand;
 import com.sujay.simulator.event.SimulationEvent;
 import com.sujay.simulator.sitemap.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 
 public class BullDozer implements Runnable {
     private final SiteMap siteMap;
     private final BlockingQueue<SimulationEvent> eventQueue;
-    private final List<Cell> visitedCells = new ArrayList<>();
+
     private Queue<Command> commandQueue;
+
+    private final List<Command> commandHistory = new ArrayList<>();
+    private final Set<Cell> visitedCells = new LinkedHashSet<>();
     private Cell currentCell = new Cell(CellType.START, new Coordinate(0, -1));
     private Orientation orientation = Orientation.EAST;
 
@@ -38,11 +40,11 @@ public class BullDozer implements Runnable {
         return siteMap;
     }
 
-    public synchronized void updateCurrentPosition(Cell currentCell) {
+    private synchronized void updateCurrentPosition(Cell currentCell) {
         this.currentCell = currentCell;
     }
 
-    public synchronized void updateOrientation(Orientation orientation) {
+    private synchronized void updateOrientation(Orientation orientation) {
         this.orientation = orientation;
     }
 
@@ -62,8 +64,155 @@ public class BullDozer implements Runnable {
         return orientation;
     }
 
-    public List<Cell> getVisitedCells() {
+    public Set<Cell> getVisitedCells() {
         return visitedCells;
+    }
+
+    public void moveBullDozer(Command command) {
+        try {
+            commandHistory.add(command);
+            Orientation orientation = getOrientation();
+            switch (orientation) {
+                case EAST:
+                    advanceEast(command);
+                    break;
+                case WEST:
+                    advanceWest(command);
+                    break;
+                case SOUTH:
+                    advanceSouth(command);
+                    break;
+                case NORTH:
+                    advanceNorth(command);
+                    break;
+                default:
+                    throw new IllegalArgumentException(String.format("Wrong orientation %s provided in the command", orientation));
+            }
+        }catch(IllegalArgumentException e) {
+            raiseQuitCommand(command);
+        }
+    }
+
+    public void turnLeft(Command command) {
+        try {
+            this.commandHistory.add(command);
+            Orientation orientation = getOrientation();
+            switch (orientation) {
+                case EAST:
+                    updateOrientation(Orientation.NORTH);
+                    break;
+                case WEST:
+                    updateOrientation(Orientation.SOUTH);
+                    break;
+                case NORTH:
+                    updateOrientation(Orientation.WEST);
+                    break;
+                case SOUTH:
+                    updateOrientation(Orientation.EAST);
+                    break;
+                default:
+                    throw new IllegalArgumentException(String.format("Wrong orientation %s provided in the command", orientation));
+            }
+            raiseCommandEvent(command);
+        }catch (IllegalArgumentException e) {
+            raiseQuitCommand(command);
+        }
+
+    }
+
+    public void turnRight(Command command) {
+        try {
+            this.commandHistory.add(command);
+            Orientation currentOrientation = getOrientation();
+            switch (currentOrientation) {
+                case EAST:
+                    updateOrientation(Orientation.SOUTH);
+                    break;
+                case WEST:
+                    updateOrientation(Orientation.NORTH);
+                    break;
+                case NORTH:
+                    updateOrientation(Orientation.EAST);
+                    break;
+                case SOUTH:
+                    updateOrientation(Orientation.WEST);
+                    break;
+                default:
+                    throw new IllegalArgumentException(String.format("Wrong orientation %s provided in the command", currentOrientation));
+            }
+            raiseCommandEvent(command);
+        }catch (IllegalArgumentException e) {
+            raiseQuitCommand(command);
+        }
+    }
+
+    private void advanceEast(Command command) {
+        advanceEastOrWest(command, Orientation.EAST);
+    }
+
+    private void advanceWest(Command command) {
+        advanceEastOrWest(command, Orientation.WEST);
+    }
+
+    private void advanceSouth(Command command) {
+        advanceSouthOrNorth(command, Orientation.SOUTH);
+    }
+
+    private void advanceNorth(Command command) {
+        advanceSouthOrNorth(command, Orientation.NORTH);
+    }
+
+    private void advanceEastOrWest(Command command, Orientation orientation) {
+        Coordinate coordinate = getCurrentPosition().getCoordinate();
+        for(int y = 1; y <= command.getArg(); y++) {
+            int newY = 0;
+            if (orientation == Orientation.EAST)
+                 newY = coordinate.getY() + y;
+            else if (orientation == Orientation.WEST)
+                newY = coordinate.getY() - y;
+
+            Coordinate newCoordinate = new Coordinate(coordinate.getX(), newY);
+            Cell newCell = siteMap.getCellAt(newCoordinate);
+            checkIfMoveAllowed(command, newCell);
+            this.visitedCells.add(newCell);
+            this.updateCurrentPosition(newCell);
+        }
+    }
+
+    private void advanceSouthOrNorth(Command command, Orientation orientation) {
+        Coordinate coordinate = getCurrentPosition().getCoordinate();
+        for(int x = 1; x <= command.getArg(); x++) {
+            int newX = 0;
+            if (orientation == Orientation.SOUTH)
+                newX = coordinate.getX() + x;
+            else if (orientation == Orientation.NORTH)
+                newX = coordinate.getX() - x;
+            Coordinate newCoordinate = new Coordinate(newX, coordinate.getY());
+            Cell newCell = siteMap.getCellAt(newCoordinate);
+            checkIfMoveAllowed(command, newCell);
+            this.visitedCells.add(newCell);
+            this.updateCurrentPosition(newCell);
+        }
+    }
+
+    private void checkIfMoveAllowed(Command command, Cell newCell) {
+        if (newCell.getCellType() == CellType.PRESERVEDTREE) {
+            raiseQuitCommand(command);
+        }else {
+            raiseCommandEvent(command);
+        }
+    }
+
+    private void raiseQuitCommand(Command command) {
+        this.addEvent(new SimulationEvent(new QuitCommand(command.getContext())));
+    }
+
+    private void raiseCommandEvent(Command command){
+        this.addEvent(new SimulationEvent(command));
+    }
+
+    public List<Command> getCommandHistory() {
+        return commandHistory;
     }
 
     @Override
